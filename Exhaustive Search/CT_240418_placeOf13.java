976797803819a06b4aeca2ba89f82e72b746c92b
me <156293610+valentde@users.noreply.github.com>
2024-04-18 23:51:25 +0900

    완전탐색2 | 06-1 02,04 기초

---
+++ Exhaustive Search/CT_240418_placeOf13.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CT_240418_placeOf13 {
+
+    public static void main(String[] args) {
+// [괄호 쌍 만들어주기 3 ](https://www.codetree.ai/missions/5/problems/pair-parentheses-3)
+        char[] c = new Scanner(System.in).nextLine().toCharArray();
+        int count=0;
+        for (int i = 0; i < c.length; i++) {
+            if(c[i] == ')') continue;
+            for (int j = i + 1; j < c.length; j++) {
+                if(c[j] == ')')
+                    ++count;
+            }
+        }
+        System.out.println(count);
+
+// [최고의 13위치 ](https://www.codetree.ai/missions/5/problems/best-place-of-13)
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] G = new int[n][n];
+        for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) G[i][j] = sc.nextInt();
+
+        int max=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int sum = G[i][j] + G[i][j+1] + G[i][j+2];
+                if(max < sum) max = sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
